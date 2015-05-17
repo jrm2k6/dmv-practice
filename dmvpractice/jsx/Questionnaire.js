141f1f2075508988439392edd404e869102803a8
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-05-17 09:06:48 -0700

    display first question

--- dmvpractice/jsx/Questionnaire.js
+++ dmvpractice/jsx/Questionnaire.js
@@ -7,24 +7,37 @@ var {
   TouchableHighlight,
   Text,
   View,
-  Component
+  Component,
+  StyleSheet
 } = React;
 
 class Questionnaire extends Component {
 	constructor(props) {
-		super(props);
-		this.state = {
-			questions: _questions
-		};
+		super(props);	
+		this.state = _questions;
 	}
 
 	render() {
 		return (
-			<Text>
-				LOL
-			</Text>
+			<View style={styles.container}>
+				<Text style={styles.txt}>
+					{this.state.questions["0"].question}
+				</Text>
+			</View>
 		)
 	}
 }
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+    	alignItems: 'center'
+	},
+
+	txt : {
+		color: 'black'
+	}
+});
 
 module.exports = Questionnaire;
